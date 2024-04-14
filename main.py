3d376ca8ed104019b7a49e9074daeb6f437e46b0
WandErer90 <131876293+WandErer90@users.noreply.github.com>
2024-04-14 10:45:59 +0100

    read file function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+path = "books/frankenstein.txt"
+
+# print contents of file at path
+
+def main (path):
+    with open(path) as f:
+        file_contents = f.read()
+        print(file_contents)
+
+main(path)
