202736dc5c18f7462646c05c7838a2775158393d
WandErer90 <131876293+WandErer90@users.noreply.github.com>
2024-04-14 11:47:20 +0100

    letter count

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
 
 def get_num_words(text):
@@ -14,5 +15,14 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 main()
