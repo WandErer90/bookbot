43efc849bb43eae5aca278c4b23c8722061921eb
WandErer90 <131876293+WandErer90@users.noreply.github.com>
2025-05-15 22:48:18 +0100

    added function to read file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(book):
+	with open(book) as f:
+		book_contents = f.read()
+	print(book_contents)
+
+def main():
+	book = "books/frankenstein.txt"
+	get_book_text(book)
+
+
+main()	
